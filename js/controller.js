163ae30413e12faa8a4c2bb743cd2ac3e1538284
Dev <dev@myhq.in>
2018-05-19 21:09:01 +0530

    swiper on detailed page

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,4 @@
-angular.module('webAppControllers', ['webAppService'])
+angular.module('webAppControllers', ['webAppService', 'ksSwiper'])
     .controller('HomeController', function($scope, $state, $window, $stateParams, ecommerceServices) {
         ecommerceServices.getProducts().then(function(response) {
             $scope.productList = response;
@@ -14,12 +14,18 @@ angular.module('webAppControllers', ['webAppService'])
     .controller('productDetailController', function($scope, $state, $window, $stateParams, ecommerceServices, $stateParams) {
         var productId = $stateParams.Id;
         var productDetail;
+        $scope.productCount = 1;
+
         ecommerceServices.getProductDetail(productId).then(function(response) {
             productDetail = response;
             setVariables();
         }, function(error) {
 
         });
+
+        $scope.onReadySwiper = function(swiper) {
+            swiper.initObservers();
+        }
 
         function setVariables() {
             $scope.detailObject = productDetail.primary_product;
@@ -33,11 +39,84 @@ angular.module('webAppControllers', ['webAppService'])
 
             $scope.productVariations = productDetail.product_variations;
 
+            setSelectedAttribute();
             // add prodcut variation -- the first object that comes with seperated selected option field -- append it here     
         }
+
+        $scope.decreementProduct = function() {
+            if (($scope.productCount - 1) >= 0) {
+                $scope.productCount = $scope.productCount - 1;
+            }
+        }
+
+        $scope.attributeSelect = function(attribute, optionSelected) {
+            for (var index in $scope.productAttributes) {
+                if ($scope.productAttributes[index]._id == attribute._id) {
+                    $scope.productAttributes[index]['selected'] = JSON.parse(JSON.stringify(optionSelected));
+                }
+            }
+
+            var selectedvariant = getSelectedVariant();
+        }
+
+        function getSelectedVariant() {
+            var allSelectedProductId = [];
+
+            for (var index in $scope.productAttributes) {
+                //collect all the selected field and then search object which contain all the fields in $scope.productVariations
+                allSelectedProductId.push($scope.productAttributes[index].selected._id);
+            }
+
+            // now allSelectedProductId contains _id of options that are currently selected
+            // now we will check the $scope.productVariations for an object which contains all this _id's
+            var selectedFieldCount = allSelectedProductId.length;
+            var matchFound = false;
+            var newProductObject;
+            for (var index in $scope.productVariations) {
+                var matchCount = 0;
+                for (var index2 in allSelectedProductId) {
+                    if ($scope.productVariations[index].sign.indexOf(allSelectedProductId[index2]) != -1) {
+                        matchCount = matchCount + 1;
+                    }
+                }
+                
+                if(matchCount == selectedFieldCount) {
+                    matchFound = true;
+                    newProductObject = JSON.parse(JSON.stringify($scope.productVariations[index]));
+                    break;
+                }
+            }
+
+            if(matchFound) {   
+                for (var key in newProductObject) {
+                    if(key != 'sign' && key != 'images') {
+                        $scope.detailObject[key] = newProductObject[key];
+                    }
+                }
+            }
+        }
+
+        function setSelectedAttribute() {
+            var tempSelectedArray = [];
+            for (var index in productDetail.selected_option_ids) {
+                tempSelectedArray.push(productDetail.options.find(function(optionObject) {
+                    return optionObject._id == productDetail.selected_option_ids[index];
+                }));
+            }
+
+            for (var index in tempSelectedArray) {
+                for (var index2 in $scope.productAttributes) {
+                    if ($scope.productAttributes[index2]._id == tempSelectedArray[index].attrib_id) {
+                        $scope.productAttributes[index2]['selected'] = JSON.parse(JSON.stringify(tempSelectedArray[index]));
+                    }
+                }
+            }
+        }
+
+        $scope.increementProduct = function() {
+            $scope.productCount = $scope.productCount + 1;
+        }
     })
-
-
 
 
 
