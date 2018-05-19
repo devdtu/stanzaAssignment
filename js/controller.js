a7de5c926a472ea4cc57a525552483e5adbb3ea3
Dev <dev@myhq.in>
2018-05-19 15:28:33 +0530

    first commit

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,75 @@
 angular.module('webAppControllers', ['webAppService'])
-    .controller('HomeController', function($scope, $state, $window, $stateParams) {
-        console.log("controller called");
+    .controller('HomeController', function($scope, $state, $window, $stateParams, ecommerceServices) {
+        ecommerceServices.getProducts().then(function(response) {
+            $scope.productList = response;
+        }, function(error) {
+
+        });
+
+        $scope.removeSpace = function(name) {
+            return name.replace(/ /g, '-');
+        }
     })
+
+    .controller('productDetailController', function($scope, $state, $window, $stateParams, ecommerceServices, $stateParams) {
+        var productId = $stateParams.Id;
+        var productDetail;
+        ecommerceServices.getProductDetail(productId).then(function(response) {
+            productDetail = response;
+            setVariables();
+        }, function(error) {
+
+        });
+
+        function setVariables() {
+            $scope.detailObject = productDetail.primary_product;
+            $scope.productAttributes = productDetail.attributes;
+            for (var index in $scope.productAttributes) {
+                var optionsArray = productDetail.options.filter(function(object) {
+                    return object.attrib_id == $scope.productAttributes[index]._id;
+                })
+                $scope.productAttributes[index]['types'] = optionsArray;
+            }
+
+            $scope.productVariations = productDetail.product_variations;
+
+            // add prodcut variation -- the first object that comes with seperated selected option field -- append it here     
+        }
+    })
+
+
+
+
+
+
+// $scope.productAttributes-- - [{
+//         "_id": "5aec58955a39460004b3f6d0",
+//         "name": "Storage",
+//         "types": [{
+//                 "_id": "5aec58955a39460004b3f6d3",
+//                 "attrib_id": "5aec58955a39460004b3f6d0",
+//                 "name": "1.5GB"
+//             },
+//             {
+//                 "_id": "5aec58955a39460004b3f6d4",
+//                 "attrib_id": "5aec58955a39460004b3f6d0",
+//                 "name": "2GB"
+//             }
+//         ]
+//     },
+//     {
+//         "_id": "5aec58955a39460004b3f6d1",
+//         "name": "Colour",
+//         "types": [{
+//                 "_id": "5aec58955a39460004b3f6d5",
+//                 "attrib_id": "5aec58955a39460004b3f6d1",
+//                 "name": "Black"
+//             },
+//             {
+//                 "_id": "5aec58955a39460004b3f6d6",
+//                 "attrib_id": "5aec58955a39460004b3f6d1",
+//                 "name": "Gold"
+//             }
+//         ]
+//     }
+// ]
--- js/services.js
+++ js/services.js
@@ -1,4 +1,23 @@
 var webApp = angular.module('webAppService', []);
 webApp.service('ecommerceServices', function($http) {
-   console.log('service running')
+    console.log('service running')
+
+    var baseUrl = 'https://assignment-appstreet.herokuapp.com/api/v1/products';
+
+    // var url = 'https://assignment-appstreet.herokuapp.com/api/v1/products?page=1';
+    // var url = 'https://assignment-appstreet.herokuapp.com/api/v1/products/5aec58965a39460004b3f6dd';
+    return {
+        getProducts: function() {
+            var url = baseUrl + '?page=1';
+            return $http.get(url, { cache: true }).then(function(data) {
+                return data.data.products;
+            });
+        },
+        getProductDetail: function(productId) {
+            var url = baseUrl + '/' + productId;
+            return $http.get(url, { cache: true }).then(function(data) {
+                return data.data;
+            });
+        }
+    }
 });
